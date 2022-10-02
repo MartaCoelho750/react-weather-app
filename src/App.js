62b6716bab42a9e173ec5add46635547818bce56
Marta Coelho <freelancer.martacoelho@gmail.com>
2022-10-02 17:09:03 +0100

    Added github repURL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1>Weather app</h1>
+      <footer>
+        <a
+          href="https://github.com/MartaCoelho750/react-weather-app"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Coded By
+        </a>{" "}
+        Marta Coelho
+      </footer>
+    </div>
+  );
 }
