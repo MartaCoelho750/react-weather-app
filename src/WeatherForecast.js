8ce2c3d77390bf4cdb76fbbe6e429a5080606b8c
Marta Coelho <freelancer.martacoelho@gmail.com>
2022-10-10 20:19:50 +0100

    Forecast 1

---
+++ src/WeatherForecast.js
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import WeatherIcon from "./WeatherIcon";
+import "./WeatherForecast.css";
+import axios from "axios";
+import WeatherForecastDay from "./WeatherForecastDay";
+
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    console.log(forecast);
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <WeatherForecastDay data={props.data.foracast[0]} />
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    let apiKey = "db38f9874aaed6906ad051b582f0daff";
+    let latitude = props.coordinates.lon;
+    let longitude = props.coordinates.lat;
+    let apiUrl = `https://pro.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  }
+}
